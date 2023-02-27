0ad02dcfa926ebe02edb3da59178e42fa5b7ec60
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-02-26 22:40:54 -0800

    i finished replacing and filling in the indexjs file with all of my necessary relationships between the models

--- models/index.js
+++ models/index.js
@@ -1,25 +1,37 @@
-const Reader = require('./Reader');
-const Book = require('./Book');
-const LibraryCard = require('./LibraryCard');
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
 
-Reader.hasOne(LibraryCard, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
+//A user can have many posts    
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
 });
 
-// A reader can have many books
-Reader.hasMany(Book, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
+//A post can have many comments
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
 });
 
-// A book belongs to a single reader
-Book.belongsTo(Reader, {
-  foreignKey: 'reader_id',
+// A user can have many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
 });
 
-LibraryCard.belongsTo(Reader, {
-  foreignKey: 'reader_id',
+//A comment can only belong to one post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
 });
 
-module.exports = { Reader, Book, LibraryCard };
+//A comment can only belong to one user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+
+
+module.exports = { User, Post, Comment };
